cfd61a01447bff8d784a4c1682fa31a10d788b03
Ruchira Wageesha <ruchira.wageesha@gmail.com>
2018-07-25 06:21:45 +0530

    updating

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var fs = require('fs');
+var util = require('util');
 
 var LOGS_DIR = process.env.LOGS_DIR || '/tmp/logs';
 
@@ -10,39 +11,71 @@ var p = function (id) {
     return LOGS_DIR + '/' + id + '.log';
 };
 
-var stringify = function (o) {
-  if (typeof o === 'string' || o instanceof String) {
-    return o;
+var serialize = function (value) {
+  if (typeof value === 'string' || value instanceof String) {
+    return value;
   }
-  if (o instanceof Error) {
-    return o.stack;
+  if (typeof value === 'number' || value instanceof Number) {
+    return value;
   }
-  return JSON.stringify(o) || '';
+  if (typeof value === 'boolean' || value instanceof Boolean) {
+    return value;
+  }
+  if (value instanceof Date) {
+    return value.toISOString();
+  }
+  if (value instanceof Error) {
+    return '\'' + value.stack.replace(/\n/g, '\\n') + '\'';
+  }
+  return JSON.stringify(value);
+}
+
+var suffix = function (args) {
+  var error;
+  var second = args.shift();
+  length = args.length;
+  if (length) {
+    error = args[length - 1]
+    error = error instanceof Error ? args.pop() : null;
+  } else if (second instanceof Error) {
+    error = second;
+    second = null;
+  }
+  var suffix = '';
+  if (error) {
+    suffix += ' error=' + serialize(error);
+  }
+  if (!second) {
+    return suffix;
+  }
+  var index = 0;
+  if (typeof second === 'string' || second instanceof String) {
+    second = second.replace(/:/g, '=');
+    suffix = util.format.apply(util, [second].concat(args)) + suffix;
+    return ' ' + suffix;
+  }
+  var prefix = '';
+
+  var prefix = '';
+  Object.keys(second).forEach(function (key) {
+    var value = second[key];
+    prefix += ' ' + key + '=' + serialize(second[key]);
+  });
+
+  return prefix + suffix;
 }
 
 var build = function (log, args) {
-    args = Array.prototype.slice.call(args);
-    var index = 1;
-    var o = stringify(args[0]);
-    o = o.replace(/%([a-z%])/g, function (match, format) {
-        // if we encounter an escaped % then don't increase the array index
-        if (match === '%%') {
-            return match;
-        }
-        var o = args[index++];
-        var out = '';
-        if (match === '%e') {
-            out += '\n';
-            o = o.stack || o;
-        }
-        out += (typeof o === 'string' || o instanceof String) ? o : JSON.stringify(o);
-        return out;
-    });
-    return log.name + ':' + o;
+  args = Array.prototype.slice.call(args);
+  var first = args.shift();
+  var splits = first.split(':');
+  var group = splits[0];
+  var action = splits[1];
+  return util.format(' module=%s group=%s action=%s%s', log.name, group, action, suffix(args));
 };
 
 var time = function(message) {
-  return new Date().toISOString() + ':' + message;
+  return new Date().toISOString() + ' ' + message;
 };
 
 var Log = function (name) {
@@ -50,23 +83,23 @@ var Log = function (name) {
 };
 
 Log.prototype.debug = function () {
-    console.info(time('debug:' + build(this, arguments)));
+    console.info(time('level=debug' + build(this, arguments)));
 };
 
 Log.prototype.info = function () {
-    console.info(time('info:' + build(this, arguments)));
+    console.info(time('level=info' + build(this, arguments)));
 };
 
 Log.prototype.warn = function () {
-    console.info(time('warn:' + build(this, arguments)));
+    console.info(time('level=warn' + build(this, arguments)));
 };
 
 Log.prototype.error = function () {
-    console.info(time('error:' + build(this, arguments)));
+    console.info(time('level=error' + build(this, arguments)));
 };
 
 Log.prototype.fatal = function (err) {
-    console.info(time('fatal:' + build(this, arguments)));
+    console.info(time('level=fatal' + build(this, arguments)));
 };
 
 Log.prototype.trace = function (err) {
